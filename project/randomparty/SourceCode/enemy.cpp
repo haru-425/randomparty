10257cedada866dcc17b05621552455a6483d50e
葉月はる <93645335+haru-425@users.noreply.github.com>
2024-10-06 11:57:41 +0900

    敵がプレイイヤーの方向に向くように
    現段階描画なし

--- project/randomparty/SourceCode/enemy.cpp
+++ project/randomparty/SourceCode/enemy.cpp
@@ -1,6 +1,7 @@
 #include"all.h"
 
 int enemy_state;
+extern PLAYER player;
 typedef enum
 {
 	APPROACH_SLOW,
@@ -51,9 +52,19 @@ void enemy_render() {
 void enemy_act() {
 	for (int i = 0; i < ENEMY_MAX; i++)
 	{
+		VECTOR2 subVector;
+		subVector.x = player.position.x - enemy[i].position.x;
+		subVector.y = player.position.y - enemy[i].position.y;
+
+		enemy[i].angle = (float)atan2(subVector.y, subVector.x);
+	}
+	for (int i = 0; i < ENEMY_MAX; i++)
+	{
 		switch (enemy[i].type)
 		{
 		case APPROACH_SLOW:
+			break;
+		case APPROACH_FAST:
 			break;
 		}
 	}
--- project/randomparty/SourceCode/enemy.h
+++ project/randomparty/SourceCode/enemy.h
@@ -7,6 +7,15 @@ class ENEMY {
 private:
 public:
 	int type;
+	float angle;
+	float speed;
+	VECTOR2 position;
+	VECTOR2 scale;
+	VECTOR2 texPos;
+	VECTOR2 texSize;
+	VECTOR2 pivot;
+	VECTOR4 color;
+
 };
 void enemy_init();
 void enemy_deinit();
