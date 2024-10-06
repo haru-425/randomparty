e74a18ebafb7f2db13412acb7d74ba078dd2220e
kyouhei2525 <hozonfuniguru1@gmail.com>
2024-10-06 17:58:37 +0900

    add bomb.cpp &bomb.h

--- project/randomparty/SourceCode/scene_title.cpp
+++ project/randomparty/SourceCode/scene_title.cpp
@@ -1,13 +1,39 @@
 #include "all.h"
+
+#define SELECT_COUNT 2              //‘I‘ğˆ‚Ì”
+
+int title_state;
+int title_timer;
+
+int select_c;
+
+Sprite* backspr;
+
+//--------------------------------------
+//  ‰Šúİ’è
+//--------------------------------------
 void title_init()
 {
+    title_state = 0;
+    title_timer = 0;
+    select_c = 0;
 }
+
+//--------------------------------------
+//  I—¹ˆ—
+//--------------------------------------
 void title_deinit()
 {
+    music::stop(3);
+
+    safe_delete(backspr);
 }
+
 void title_update()
 {
 }
 void title_render()
 {
+    // ‰æ–Ê‚ğÂ‚Å“h‚è‚Â‚Ô‚·
+    GameLib::clear(0.3f, 0.5f, 1.0f);
 }
