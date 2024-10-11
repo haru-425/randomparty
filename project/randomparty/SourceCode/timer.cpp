c6731411c0d7f74d254c1a8e3e5c44c2fa20641e
masa433 <masa1107s@icloud.com>
2024-10-11 14:57:31 +0900

    更新
    タイマー表示更新

--- project/randomparty/SourceCode/timer.cpp
+++ project/randomparty/SourceCode/timer.cpp
@@ -20,12 +20,12 @@ void timer_update() {
     frame_count++;
 
     // 1フレームを「1秒」に見立てて60フレーム経過ごとに1秒減らす
-    if (frame_count  >= 0) {
+    if (frame_count  >= 60) {
         --remaining_time;   // 残り時間を1秒減少
     }
 
     // 残り時間が0以下になったらタイマー停止
-    if (remaining_time < 0) {
+    if (remaining_time < 1) {
         remaining_time = 0;
         game_end();
     }
@@ -34,6 +34,12 @@ void timer_update() {
 void timer_render() {
     // 残り時間を秒単位で表示する
     int seconds = remaining_time / 60;  // フレームカウントで秒単位に変換
+
+    // 秒数が1未満の場合は1秒を表示する
+    if (seconds < 1) {
+        seconds = 1;
+    }
+
     std::string time_text = "TIME: " + std::to_string(seconds) ;
     text_out(1, time_text.c_str(), 0, 0, 2, 2, 1, 1, 1, 1);  // 座標 (200, 0) にテキスト描画
 }
