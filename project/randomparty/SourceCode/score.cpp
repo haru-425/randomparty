fe0a186bfa59be6fdb1ad7109168fbd66716e745
kyouhei2525 <hozonfuniguru1@gmail.com>
2024-10-12 17:52:19 +0900

    add

---
+++ project/randomparty/SourceCode/score.cpp
@@ -0,0 +1,32 @@
+#include <math.h>
+#include "all.h"
+using namespace std;
+
+
+void SCORE::near_score()
+{
+	time++;
+	for (int i = 0; i < ENEMY_MAX; i++)
+	{
+		dx = player.position.x - enemy[i].position.x;
+		dy = player.position.y - enemy[i].position.y;
+		distance[1] = sqrt(dx * dx + dy * dy);
+		if (distance[1] < distance[0])
+		{
+			distance[0] = distance[1];
+		}
+	}
+	if (300 - distance[0] > 0 && time % 10 == 0)
+	{
+		near_total += (300 - distance[0]) / 50;
+		total_score += (300 - distance[0]) / 50;
+	}
+	debug::setString("%d", near_total);
+	distance[0] = 1000000;
+}
+
+void SCORE::enemy_kill_point()
+{
+	enemy_kill++;
+	total_score += ENEMY_KILL_POINT;
+}
--- project/randomparty/SourceCode/score.h
+++ project/randomparty/SourceCode/score.h
@@ -2,6 +2,8 @@
 #include <math.h>
 #include "all.h"
 
+#define ENEMY_KILL_POINT 100
+
 extern PLAYER player;
 extern ENEMY enemy[ENEMY_MAX];
 
@@ -17,24 +19,18 @@ public:
 	SCORE() :total_score(0), enemy_kill(0), near_total(0) {}
 	~SCORE() {}
 	float distance[2] = { 1000000,0 }, dx, dy;
-	void near_score()
+	void near_score();
+	void enemy_kill_point();
+	int score_get()
 	{
-		time++;
-		for (int i = 0; i < ENEMY_MAX; i++)
-		{
-			dx = player.position.x - enemy[i].position.x;
-			dy = player.position.y - enemy[i].position.y;
-			distance[1] = sqrt(dx * dx + dy * dy);
-			if (distance[1]< distance[0])
-			{
-				distance[0] = distance[1];
-			}
-		}
-		if (300 - distance[0]>0&& time %60 == 0)
-		{
-			near_total += (300 - distance[0])/50;
-		}
-		debug::setString("%d", near_total);
-		distance[0] = 1000000;
+		return total_score;
+	}
+	int kill_score_get()
+	{
+		return enemy_kill;
+	}
+	int distance_score_get()
+	{
+		return near_total;
 	}
 };
