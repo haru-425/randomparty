44f3b2c508d9c598ef0cebfa57576f636c58850e
葉月はる <93645335+haru-425@users.noreply.github.com>
2024-10-24 17:51:48 +0900

    Update

--- project/randomparty/SourceCode/enemy.cpp
+++ project/randomparty/SourceCode/enemy.cpp
@@ -18,193 +18,192 @@ Sprite* sprTrackingRange;  // 索敵範囲の見た目
 
 // 敵の初期化関数
 void enemy_init() {
-    enemy_state = 0;    // 敵の状態を初期化
-    enemy_timer = 0;    // 敵のタイマーを初期化
+	enemy_state = 0;    // 敵の状態を初期化
+	enemy_timer = 0;    // 敵のタイマーを初期化
 }
 
 // 敵の終了処理。メモリの解放など
 void enemy_deinit() {
-    safe_delete(sprEnemy);
-    safe_delete(sprEnemyCore);
-    safe_delete(sprTrackingRange);
+	safe_delete(sprEnemy);
+	safe_delete(sprEnemyCore);
+	safe_delete(sprTrackingRange);
 }
 
 // 敵の更新処理
 void enemy_update() {
-    // 敵が無効化されている場合、処理をスキップ
-    if (!enemies_active) return;
+	// 敵が無効化されている場合、処理をスキップ
+	if (!enemies_active) return;
 
-    enemy_timer++;
+	enemy_timer++;
 
-    // 敵の状態に応じた処理
-    switch (enemy_state) {
-    case 0:
-        // 初期設定: スプライトの読み込み
-        sprEnemy = sprite_load(L"./Data/Images/enemy_approach_slow.png");
-        sprEnemyCore = sprite_load(L"./Data/Images/enemyCore.png");
-        sprTrackingRange = sprite_load(L"./Data/Images/trackingRange.png");
-        ++enemy_state; // 次の状態に移行
-        /*fallthrough*/
+	// 敵の状態に応じた処理
+	switch (enemy_state) {
+	case 0:
+		// 初期設定: スプライトの読み込み
+		sprEnemy = sprite_load(L"./Data/Images/enemy_approach_slow.png");
+		sprEnemyCore = sprite_load(L"./Data/Images/enemyCore.png");
+		sprTrackingRange = sprite_load(L"./Data/Images/trackingRange.png");
+		++enemy_state; // 次の状態に移行
+		/*fallthrough*/
 
-    case 1:
-        // パラメータの設定: 敵をリセットし、待機番号を設定
-        for (int i = 0; i < ENEMY_MAX; i++) {
-            enemy[i].reset();
-            enemy[i].waitNum = i + 1;  // 順番に待機するための番号
-        }
-        ++enemy_state; // 次の状態に移行
-        /*fallthrough*/
+	case 1:
+		// パラメータの設定: 敵をリセットし、待機番号を設定
+		for (int i = 0; i < ENEMY_MAX; i++) {
+			enemy[i].reset();
+			enemy[i].waitNum = i + 1;  // 順番に待機するための番号
+		}
+		++enemy_state; // 次の状態に移行
+		/*fallthrough*/
 
-    case 2:
-        // 通常時の更新処理: 一定間隔で敵を生成
-        enemy_timer++;
-        if (enemy_timer > ENEMY_SPAWN_RATE) {
-            for (int i = 0; i < ENEMY_MAX; i++) {
-                enemy[i].waitNum--;
-                if (enemy[i].waitNum < 0) {
-                    enemy[i].waitNum = -1;
-                }
-                if (enemy[i].waitNum == 0) {
-                    enemy[i].set();  // 敵をセット
-                    enemy[i].waitNum--;
-                }
-            }
-            enemy_timer = 0;  // タイマーをリセット
-        }
+	case 2:
+		// 通常時の更新処理: 一定間隔で敵を生成
+		enemy_timer++;
+		if (enemy_timer > ENEMY_SPAWN_RATE) {
+			for (int i = 0; i < ENEMY_MAX; i++) {
+				enemy[i].waitNum--;
+				if (enemy[i].waitNum < 0) {
+					enemy[i].waitNum = -1;
+				}
+				if (enemy[i].waitNum == 0) {
+					enemy[i].set();  // 敵をセット
+					enemy[i].waitNum--;
+				}
+			}
+			enemy_timer = 0;  // タイマーをリセット
+		}
 
-        // 敵の動作処理
-        enemy_act();
-        break;
-    }
+		// 敵の動作処理
+		enemy_act();
+		break;
+	}
 }
 
 // 敵の描画処理
 void enemy_render() {
-    // 敵が無効化されている場合、描画をスキップ
-    if (!enemies_active) return;
+	// 敵が無効化されている場合、描画をスキップ
+	if (!enemies_active) return;
 
-    for (int i = 0; i < ENEMY_MAX; i++) {
-        // 敵が待機状態でない場合、描画を行う
-        if (enemy[i].waitNum <= 0) {
-            // 画面内にいる場合にのみ描画
-            if (enemy[i].position.x >= 0 && enemy[i].position.x <= SCREEN_W && enemy[i].position.y >= 0 && enemy[i].position.y <= SCREEN_H) {
-                switch (enemy[i].type) {
-                case REBOUND:
-                    // 反射するタイプの敵の軌跡を描画
-                    for (int j = 0; j < 50; j++) {
-                        float posX, posY;
-                        posX = enemy[i].position.x + cosf(enemy[i].angle) * j * 25;
-                        posY = enemy[i].position.y + sinf(enemy[i].angle) * j * 25;
+	for (int i = 0; i < ENEMY_MAX; i++) {
+		// 敵が待機状態でない場合、描画を行う
+		if (enemy[i].waitNum <= 0) {
+			// 画面内にいる場合にのみ描画
+			if (enemy[i].position.x >= 0 && enemy[i].position.x <= SCREEN_W && enemy[i].position.y >= 0 && enemy[i].position.y <= SCREEN_H) {
+				switch (enemy[i].type) {
+				case REBOUND:
+					// 反射するタイプの敵の軌跡を描画
+					for (int j = 0; j < 50; j++) {
+						float posX, posY;
+						posX = enemy[i].position.x + cosf(enemy[i].angle) * j * 25;
+						posY = enemy[i].position.y + sinf(enemy[i].angle) * j * 25;
 
-                        // 画面外に出た場合の処理
-                        if (posX < 0) posX *= -1;
-                        if (posY < 0) posY *= -1;
-                        if (posX > SCREEN_W) posX = SCREEN_W - (posX - SCREEN_W);
-                        if (posY > SCREEN_H) posY = SCREEN_H - (posY - SCREEN_H);
+						// 画面外に出た場合の処理
+						if (posX < 0) posX *= -1;
+						if (posY < 0) posY *= -1;
+						if (posX > SCREEN_W) posX = SCREEN_W - (posX - SCREEN_W);
+						if (posY > SCREEN_H) posY = SCREEN_H - (posY - SCREEN_H);
 
-                        primitive::circle(posX, posY, 5, 1, 1, ToRadian(0), 1, 1, 1, 0.3f);
-                    }
-                    break;
-                }
-            }
-        }
-
-        // 敵本体とそのコアの描画
-        sprite_render(sprEnemy, enemy[i].position.x, enemy[i].position.y, enemy[i].scale.x, enemy[i].scale.y, enemy[i].texPos.x, enemy[i].texPos.y, enemy[i].texSize.x, enemy[i].texSize.y, enemy[i].pivot.x, enemy[i].pivot.y, enemy[i].angle, enemy[i].color.x, enemy[i].color.y, enemy[i].color.z, enemy[i].color.w);
-        sprite_render(sprEnemyCore, enemy[i].position.x, enemy[i].position.y, enemy[i].scale.x, enemy[i].scale.y, 0, 0, ENEMY_CORE_TEX_W, ENEMY_CORE_TEX_H, ENEMY_CORE_PIVOT_X, ENEMY_CORE_PIVOT_Y, enemy[i].angle, enemy[i].color.x, enemy[i].color.y, enemy[i].color.z, enemy[i].color.w);
-    }
+						primitive::circle(posX, posY, 5, 1, 1, ToRadian(0), 1, 1, 1, 0.3f);
+					}
+					break;
+				}
+			}
+		}
+		// 敵本体とそのコアの描画
+		sprite_render(sprEnemy, enemy[i].position.x, enemy[i].position.y, enemy[i].scale.x, enemy[i].scale.y, enemy[i].texPos.x, enemy[i].texPos.y, enemy[i].texSize.x, enemy[i].texSize.y, enemy[i].pivot.x, enemy[i].pivot.y, enemy[i].angle, enemy[i].color.x, enemy[i].color.y, enemy[i].color.z, enemy[i].color.w);
+		sprite_render(sprEnemyCore, enemy[i].position.x, enemy[i].position.y, enemy[i].scale.x, enemy[i].scale.y, 0, 0, ENEMY_CORE_TEX_W, ENEMY_CORE_TEX_H, ENEMY_CORE_PIVOT_X, ENEMY_CORE_PIVOT_Y, enemy[i].angle, enemy[i].color.x, enemy[i].color.y, enemy[i].color.z, enemy[i].color.w);
+	}
 }
 
 // 敵の行動処理
 void enemy_act() {
-    // 敵が無効化されている場合、動作処理をスキップ
-    if (!enemies_active) return;
+	// 敵が無効化されている場合、動作処理をスキップ
+	if (!enemies_active) return;
 
-    for (int i = 0; i < ENEMY_MAX; i++) {
-        float speedX, speedY;
+	for (int i = 0; i < ENEMY_MAX; i++) {
+		float speedX, speedY;
 
-        if (enemy[i].waitNum <= 0) {
-            // プレイヤーとの距離を計算
-            float DistanceX = player.position.x - enemy[i].position.x;
-            float DistanceY = player.position.y - enemy[i].position.y;
-            float Distance = sqrtf(DistanceX * DistanceX + DistanceY * DistanceY);
+		if (enemy[i].waitNum <= 0) {
+			// プレイヤーとの距離を計算
+			float DistanceX = player.position.x - enemy[i].position.x;
+			float DistanceY = player.position.y - enemy[i].position.y;
+			float Distance = sqrtf(DistanceX * DistanceX + DistanceY * DistanceY);
 
-            // 敵のタイマーが0未満なら索敵範囲に基づいて行動
-            if (enemy[i].timer < 0) {
-                if (Distance <= enemy[i].trackingRange) {  // 索敵範囲内かどうか
-                    switch (enemy[i].type) {
-                    case APPROACH_SLOW:
-                        // 遅い動きの敵
-                        enemy[i].angle = tracking(player.position, enemy[i].position);
-                        speedX = cosf(enemy[i].angle) * 1;
-                        speedY = sinf(enemy[i].angle) * 1;
-                        enemy[i].position.x += speedX * enemy[i].speed;
-                        enemy[i].position.y += speedY * enemy[i].speed;
-                        break;
-                    case APPROACH_FAST:
-                        // 速い動きの敵
-                        enemy[i].angle = tracking(player.position, enemy[i].position);
-                        speedX = cosf(enemy[i].angle) * 1;
-                        speedY = sinf(enemy[i].angle) * 1;
-                        enemy[i].position.x += speedX * enemy[i].speed;
-                        enemy[i].position.y += speedY * enemy[i].speed;
-                        break;
-                    case CHARGE:
-                        // 一定距離内に近づくとチャージ状態になる敵
-                        if (Distance <= enemy[i].trackingRange / 2) {
-                            if (enemy[i].state == 0)
-                                enemy[i].angle += ToRadian(float(-10 + rand() % 20));  // 多少ランダムな動き
-                            enemy[i].speed = 5;
-                            enemy[i].state = 1;
-                        }
-                        else {
-                            enemy[i].speed = 3;
-                            enemy[i].angle = tracking(player.position, enemy[i].position);
-                            enemy[i].state = 0;
-                        }
-                        speedX = cosf(enemy[i].angle) * 1;
-                        speedY = sinf(enemy[i].angle) * 1;
-                        enemy[i].position.x += speedX * enemy[i].speed;
-                        enemy[i].position.y += speedY * enemy[i].speed;
-                        break;
-                    case REBOUND:
-                        // 反射するタイプの敵
-                        speedX = cosf(enemy[i].angle) * 1;
-                        speedY = sinf(enemy[i].angle) * 1;
-                        enemy[i].position.x += speedX * enemy[i].speed * 0.5f;
-                        enemy[i].position.y += speedY * enemy[i].speed * 0.5f;
-                        break;
-                    }
-                }
-                else {
-                    // 索敵範囲外の場合、ランダムな動きをする
-                    enemy[i].angle += ToRadian(rand() % 7 - 3);
-                    speedX = cosf(enemy[i].angle) * 1;
-                    speedY = sinf(enemy[i].angle) * 1;
-                    enemy[i].position.x += speedX * enemy[i].speed * 0.5f;
-                    enemy[i].position.y += speedY * enemy[i].speed * 0.5f;
-                    enemy[i].state = 0;
-                }
+			// 敵のタイマーが0未満なら索敵範囲に基づいて行動
+			if (enemy[i].timer < 0) {
+				if (Distance <= enemy[i].trackingRange) {  // 索敵範囲内かどうか
+					switch (enemy[i].type) {
+					case APPROACH_SLOW:
+						// 遅い動きの敵
+						enemy[i].angle = tracking(player.position, enemy[i].position);
+						speedX = cosf(enemy[i].angle) * 1;
+						speedY = sinf(enemy[i].angle) * 1;
+						enemy[i].position.x += speedX * enemy[i].speed;
+						enemy[i].position.y += speedY * enemy[i].speed;
+						break;
+					case APPROACH_FAST:
+						// 速い動きの敵
+						enemy[i].angle = tracking(player.position, enemy[i].position);
+						speedX = cosf(enemy[i].angle) * 1;
+						speedY = sinf(enemy[i].angle) * 1;
+						enemy[i].position.x += speedX * enemy[i].speed;
+						enemy[i].position.y += speedY * enemy[i].speed;
+						break;
+					case CHARGE:
+						// 一定距離内に近づくとチャージ状態になる敵
+						if (Distance <= enemy[i].trackingRange / 2) {
+							if (enemy[i].state == 0)
+								enemy[i].angle += ToRadian(float(-10 + rand() % 20));  // 多少ランダムな動き
+							enemy[i].speed = 5;
+							enemy[i].state = 1;
+						}
+						else {
+							enemy[i].speed = 3;
+							enemy[i].angle = tracking(player.position, enemy[i].position);
+							enemy[i].state = 0;
+						}
+						speedX = cosf(enemy[i].angle) * 1;
+						speedY = sinf(enemy[i].angle) * 1;
+						enemy[i].position.x += speedX * enemy[i].speed;
+						enemy[i].position.y += speedY * enemy[i].speed;
+						break;
+					case REBOUND:
+						// 反射するタイプの敵
+						speedX = cosf(enemy[i].angle) * 1;
+						speedY = sinf(enemy[i].angle) * 1;
+						enemy[i].position.x += speedX * enemy[i].speed * 0.5f;
+						enemy[i].position.y += speedY * enemy[i].speed * 0.5f;
+						break;
+					}
+				}
+				else {
+					// 索敵範囲外の場合、ランダムな動きをする
+					enemy[i].angle += ToRadian(rand() % 7 - 3);
+					speedX = cosf(enemy[i].angle) * 1;
+					speedY = sinf(enemy[i].angle) * 1;
+					enemy[i].position.x += speedX * enemy[i].speed * 0.5f;
+					enemy[i].position.y += speedY * enemy[i].speed * 0.5f;
+					enemy[i].state = 0;
+				}
 
-                // 画面外に出た場合の反射処理
-                if (enemy[i].position.x < 0 || enemy[i].position.x > SCREEN_W) {
-                    speedX *= -1;
-                    enemy[i].angle = (float)atan2(speedY, speedX);
-                }
-                if (enemy[i].position.y < 0 || enemy[i].position.y > SCREEN_H) {
-                    speedY *= -1;
-                    enemy[i].angle = (float)atan2(speedY, speedX);
-                }
-            }
-            else {
-                // 敵のタイマーが有効な場合、追跡行動を行う
-                enemy[i].angle = tracking(player.position, enemy[i].position);
-                speedX = cosf(enemy[i].angle) * 1;
-                speedY = sinf(enemy[i].angle) * 1;
-                enemy[i].position.x += speedX * 2;
-                enemy[i].position.y += speedY * 2;
-                enemy[i].timer--;
-            }
-        }
-    }
+				// 画面外に出た場合の反射処理
+				if (enemy[i].position.x < 0 || enemy[i].position.x > SCREEN_W) {
+					speedX *= -1;
+					enemy[i].angle = (float)atan2(speedY, speedX);
+				}
+				if (enemy[i].position.y < 0 || enemy[i].position.y > SCREEN_H) {
+					speedY *= -1;
+					enemy[i].angle = (float)atan2(speedY, speedX);
+				}
+			}
+			else {
+				// 敵のタイマーが有効な場合、追跡行動を行う
+				enemy[i].angle = tracking(player.position, enemy[i].position);
+				speedX = cosf(enemy[i].angle) * 1;
+				speedY = sinf(enemy[i].angle) * 1;
+				enemy[i].position.x += speedX * 2;
+				enemy[i].position.y += speedY * 2;
+				enemy[i].timer--;
+			}
+		}
+	}
 }
---
+++ ▼【１年生／テーマ『記録を競い合う』／2024年度後期／ゲーム制作】_提出について/②sv23提出/ツール/CollectAnimLayer/CollectAnimLayer.jsx
@@ -0,0 +1,169 @@
+﻿// ヒストリーのまとめとmain関数の呼び出し
+app.activeDocument.suspendHistory("CollectAnimation", "main()");
+
+
+//============================================================================
+function main() {
+	//　①　UIの本体となるウィンドウを作る
+	var win = new Window("dialog", "Collect Animation");
+
+	win.sText = win.add("statictext",[20,10,450,10+25], "選択中の画像を１枚に並べたレイヤーを作成します")
+	win.sText = win.add("statictext",[20,10,450,10+25], "事前にレイヤーを下から順番に並べておき、出力したいレイヤーを選択しておいてください")
+	win.slider = win.add("slider",[30,30,350,40],1,1,16);
+	win.sText = win.add("statictext",[20,10,360,10+25], "横に画像を並べる数を上記スライドで指定してください。　　横の数："+win.slider.value);
+	win.slider.onChange = function ()
+	{
+		win.slider.value = Math.round(win.slider.value);
+		win.sText.text = "横に画像を並べる数を上記スライドで指定してください。　　横の数："+win.slider.value;
+	}
+
+	var planeBtn1 = win.add("Button", undefined, "実行して結合する");
+	planeBtn1.onClick=function()
+	{
+		win.close();
+		makeCollectAnimLayer(true, win.slider.value);
+	}
+
+	var planeBtn2 = win.add("Button", undefined, "実行して結合しない");
+	planeBtn2.onClick=function()
+	{
+		win.close();
+		makeCollectAnimLayer(false, win.slider.value);
+	}
+	
+	//　ウィンドウを表示する
+	win.show();
+}
+
+function makeCollectAnimLayer(marge, widthCount)
+{
+	if (app.documents.length)
+	{
+		var docRef = app.activeDocument;
+		if (docRef.layers.length)
+		{
+			var newDoc;
+			var newLayer;
+			
+			// 選択されているレイヤーを取得する
+			var selected = getSelectedLayerInfo();
+			if( selected.length > 1 )
+			{
+				width = app.activeDocument.width;
+				height = app.activeDocument.height;
+				
+				for (var i = 0; i < selected.length; i++)
+				{
+					// 選択されているレイヤーのIdxを取得する
+					var layer = getLayerByIndex(selected[i]);
+					
+					if( i == 0 )
+					{
+						newDoc = app.documents.add(width * widthCount, height*Math.ceil(selected.length/widthCount), layer.resolution, "CollectAnimLayer", NewDocumentMode.RGB, DocumentFill.TRANSPARENT);
+						newLayer = app.activeDocument.activeLayer;						
+						app.activeDocument = docRef;
+					}
+				
+					var nowLayer = layer.duplicate (newDoc);
+					app.activeDocument = newDoc;
+
+					nowLayer.translate(width*( i % widthCount ), height * parseInt( i / widthCount ));
+					app.activeDocument = docRef;
+				}
+			
+				app.activeDocument = newDoc;
+				if(marge)
+				{
+					newDoc.mergeVisibleLayers();
+				}
+				else
+				{
+					newLayer.remove();
+				}
+			}
+			else
+			{
+				alert("二つ以上のレイヤーを選択してください");
+			}
+		}
+	}
+}
+
+function getSelectedLayerInfo()
+{
+	var selectedLayers = new Array;
+
+	var ref = new ActionReference();
+	ref.putEnumerated(charIDToTypeID("Dcmn"), charIDToTypeID("Ordn"), charIDToTypeID("Trgt"));
+
+	var desc = executeActionGet(ref);
+	if (desc.hasKey(stringIDToTypeID('targetLayers')))
+	{
+		desc = desc.getList(stringIDToTypeID('targetLayers'));
+
+		var c = desc.count
+		var selectedLayers = new Array();
+
+		for (var i = 0; i < c; i++)
+		{
+			try {
+				activeDocument.backgroundLayer;
+				selectedLayers.push(desc.getReference(i).getIndex());
+			} catch (e) {
+				selectedLayers.push(desc.getReference(i).getIndex() + 1);
+			}
+		}
+	}
+	else
+	{
+		var ref = new ActionReference();
+		ref.putProperty(charIDToTypeID("Prpr"), charIDToTypeID("ItmI"));
+		ref.putEnumerated(charIDToTypeID("Lyr "), charIDToTypeID("Ordn"), charIDToTypeID("Trgt"));
+
+		try {
+			activeDocument.backgroundLayer;
+			selectedLayers.push(executeActionGet(ref).getInteger(charIDToTypeID("ItmI")) - 1);
+		} catch (e) {
+			selectedLayers.push(executeActionGet(ref).getInteger(charIDToTypeID("ItmI")));
+		}
+
+		var vis = app.activeDocument.activeLayer.visible;
+		if (vis == true) app.activeDocument.activeLayer.visible = false;
+
+		var desc9 = new ActionDescriptor();
+		var list9 = new ActionList();
+		var ref9 = new ActionReference();
+
+		ref9.putEnumerated(charIDToTypeID('Lyr '), charIDToTypeID('Ordn'), charIDToTypeID('Trgt'));
+		list9.putReference(ref9);
+		desc9.putList(charIDToTypeID('null'), list9);
+		executeAction(charIDToTypeID('Shw '), desc9, DialogModes.NO);
+
+		if (app.activeDocument.activeLayer.visible == false) selectedLayers.shift();
+
+		app.activeDocument.activeLayer.visible = vis;
+	}
+
+	return selectedLayers;
+}
+
+
+function getLayerByIndex(idx)
+{
+	if (idx == 0) return;
+
+	if (idx.length != undefined)
+    {
+		idx = idx[0];
+	}
+
+	// execute selection
+	var desc = new ActionDescriptor();
+	var ref = new ActionReference();
+
+	ref.putIndex(charIDToTypeID('Lyr '), idx);
+	desc.putReference(charIDToTypeID('null'), ref);
+	executeAction(charIDToTypeID('slct'), desc, DialogModes.NO);
+
+	return app.activeDocument.activeLayer;
+}
